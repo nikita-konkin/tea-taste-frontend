87724d506e0a85c141647f931eebc5311f4a8c25
Nikita Konkin <niki-konkin@yandex.ru>
2022-08-31 23:40:37 +0300

    slider box - in progress

--- src/components/SelectBox.jsx
+++ src/components/SelectBox.jsx
@@ -3,18 +3,11 @@ import Select from 'react-select';
 
 function SelectBox(props) {
 
-	const options = [
-  { label: "Test1", value: 1 },
-  { label: "Test2", value: 3 },
-  { label: "Test3", value: 2 },
-  { label: "Test4", value: 3 },
-	];
-
 
 	return(
 		<div className="selectbox">
 			<p className="selectbox__name">{props.boxName}</p>
-			<Select	options={options} />
+			<Select	options={props.options} />
 		</div>
 		)
 }
--- src/components/Teaform.jsx
+++ src/components/Teaform.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Header from './Header.jsx'
 import SelectBox from './SelectBox.jsx';
+import SliderBox from './SliderBox.jsx';
+
 
 function Teaform(argument) {
 
@@ -17,7 +19,12 @@ function Teaform(argument) {
 			<Header />
 			<form className="form">
 				<h3>Шаг 1 основаня информация</h3>
-				<SelectBox boxName='Название чая'/>
+				<SelectBox boxName='Название чая' options={options}/>
+				<SliderBox sliderName='Вес чая при заваривании'/>
+				<SelectBox boxName='Тип чая' options={options}/>
+				<SelectBox boxName='Вода' options={options}/>
+				<SelectBox boxName='Посуда' options={options}/>
+				<SelectBox boxName='Метод заваривания' options={options}/>
 
 			</form>
 		</>
