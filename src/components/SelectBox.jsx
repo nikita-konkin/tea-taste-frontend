8f92ad5242ab9915f0d39d372bd2a8f8a0c8ca00
Nikita Konkin <niki-konkin@yandex.ru>
2022-09-12 22:29:52 +0300

    mui autocomplete - in progress

--- src/components/SelectBox.jsx
+++ src/components/SelectBox.jsx
@@ -1,36 +1,25 @@
 import React, {useState} from 'react';
 import Select from 'react-select';
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
+import PropTypes from "prop-types";
+import Autocomplete, { autocompleteClasses } from "@mui/material/Autocomplete";
+// import TextField from "@mui/material/TextField";
+import { Popper, TextField} from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-const useStyles = styled((theme) => ({
-  root: {
-    "& .MuiInputLabel-outlined:not(.MuiInputLabel-shrink)": {
-      // Default transform is "translate(14px, 20px) scale(1)""
-      // This lines up the label with the initial cursor position in the input
-      // after changing its padding-left.
-      transform: "translate(34px, 20px) scale(1);"
-    }
+
+
+const StyledPopper = styled(Popper)({
+  [`& .${autocompleteClasses.listbox}`]: {
+    boxSizing: 'border-box',
+    '& ul': {
+      padding: 0,
+      margin: 0,
+      color: 'red'
+    },
   },
-  inputRoot: {
-    color: "purple",
-    // This matches the specificity of the default styles at https://github.com/mui-org/material-ui/blob/v4.11.3/packages/material-ui-lab/src/Autocomplete/Autocomplete.js#L90
-    '&[class*="MuiOutlinedInput-root"] .MuiAutocomplete-input:first-child': {
-      // Default left padding is 6px
-      paddingLeft: 26
-    },
-    "& .MuiOutlinedInput-notchedOutline": {
-      borderColor: "green"
-    },
-    "&:hover .MuiOutlinedInput-notchedOutline": {
-      borderColor: "red"
-    },
-    "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
-      borderColor: "purple"
-    }
-  }
-}));
+});
+
+
 
 const StyledTextField = styled(TextField)({
 	width: '100%',
@@ -38,15 +27,16 @@ const StyledTextField = styled(TextField)({
     color: "#ffffff",
     '& .MuiInput-underline::before': {
       // border: '8px solid',
-      borderBottomColor: 'rgba(255, 255, 255, 0.42)',
+      // borderBottomColor: 'rgba(0, 0, 0, 0.42)',
       // width: '28px',
 
     },
   }
 });
 
+
 function SelectBox(props) {
-	const classes = useStyles()
+	// const classes = useStyles()
 	return(
 
 		// <div className="selectbox">
@@ -55,15 +45,20 @@ function SelectBox(props) {
 		// </div>
 
     <Autocomplete
-    classes = {classes}
+    // classes = {classes}
       options={props.options}
       getOptionLabel={(option) => option.title}
       // style={{ width: 300 }}
-     //  sx={{
-     //  "& .css-1a1fmpi-MuiInputBase-root-MuiInput-root::before":
-     //      // { borderBottomColor: "blue" }
-     //      { borderBottomColor: "grey"},
-    	// }}
+      sx={{
+      "& .MuiInputBase-root::before":
+          { borderBottomColor: "white"},
+      "& .MuiInputBase-root:hover:not(.Mui-disabled):before":
+          { borderBottomColor: "white"},
+      "& .MuiInputBase-root::after":
+          { borderBottomColor: "#00ffaf"},
+      "& .MuiSvgIcon-root":
+          { color: "white"},
+    	}}
 			renderInput={(params) => (
 				<StyledTextField
 					{...params}
@@ -71,16 +66,12 @@ function SelectBox(props) {
 					variant="standard"
 				/>
 			)}
-      // renderInput={(params) => {
-      //   return (
-      //     <TextField
-      //       {...params}
-      //       label={props.boxName}
-      //       variant="outlined"
-      //       fullWidth
-      //     />
-      //   );
-      // }}
+			// PopperComponent={StyledPopper}
+		  renderOption={(props, option, state) => {
+		    return (
+		      <StyledPopper>{`${option.title}`}</StyledPopper>
+		    ); //display value
+		  }}
     />
 
 		)
